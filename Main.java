5159bcfcac3460b888c9ec85224df439b7bd4da6
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2021-10-03 19:12:22 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
 class Main {
+  public static int input() {
+    Scanner scan = new Scanner(System.in);
+    return scan.nextInt();
+  }
+  
+  public static void print() {
+    System.out.println("JUnit is so cool!");
+  }
+  
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
